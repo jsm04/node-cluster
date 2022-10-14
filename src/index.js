e25126afbe04f598e99387ba5c9113007b36abf7
jsm04 <josemoretti01@gmail.com>
2022-10-13 22:07:03 -0300

    add nginx.conf

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-	res.status(200).send({ status: 'ok', message: 'Route working' })
+	res.status(200).send({ status: 'ok', message: `Route working on ${process.env.PORT}` })
 })
 
 app.listen(process.env.PORT, () => {
 	console.log(`Listening on PORT`)
 })
+
